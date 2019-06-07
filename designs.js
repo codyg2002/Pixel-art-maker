103f88614bdd3db4718f2400cc36fac0b6a56cd0
codyg2002 <50786509+codyg2002@users.noreply.github.com>
2019-06-06 22:26:10 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,30 +1,30 @@
 // Reference
 // - Knowledge from udacity.com (https://knowledge.udacity.com/)
 // Select color input
-const color = document.getElementById("colorPicker");
+const color = document.getElementById('colorPicker');
 
 // Select size input
-const height = document.getElementById("inputHeight").value;
-const width = document.getElementById("inputWidth").value;
-const sizePicker = document.getElementById("sizePicker");
-const canvas = document.getElementById("pixelCanvas");
+const height = document.getElementById('inputHeight').value;
+const width = document.getElementById('inputWidth').value;
+const sizePicker = document.getElementById('sizePicker');
+const canvas = document.getElementById('pixelCanvas');
 // When size is submitted by the user, call makeGrid()
-sizePicker.addEventListener("submit", function(submit){
+sizePicker.addEventListener('submit', function(submit){
      submit.preventDefault();
      canvas.innerHTML = null;
      makeGrid(); 
 });
 
 function makeGrid() {
-     const height = document.getElementById("inputHeight").value;
-     const width = document.getElementById("inputWidth").value;
+     const height = document.getElementById('inputHeight').value;
+     const width = document.getElementById('inputWidth').value;
      for (let r = 1; r <= height; r++){
-        let row = document.createElement("tr");
+        let row = document.createElement('tr');
         canvas.appendChild(row);
         for (let c = 1; c <= width; c++){
-            let column = document.createElement("td");
+            let column = document.createElement('td');
             canvas.appendChild(column);
-            column.addEventListener("click", function(){
+            column.addEventListener('click', function(){
                column.style.backgroundColor = color.value
          })
      }
